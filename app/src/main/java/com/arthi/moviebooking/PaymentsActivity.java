7579c79851723afe0827f9adbbb0b1d2eaa838f1
arthi-abilasha <arthiabilasha@gmail.com>
2019-12-03 21:12:33 -0600

    Initial Commit

--- app/src/main/java/com/arthi/moviebooking/PaymentsActivity.java
+++ app/src/main/java/com/arthi/moviebooking/PaymentsActivity.java
@@ -2,14 +2,58 @@ package com.arthi.moviebooking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PaymentsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payments);
+
+        final Context context = this;
+        @Override
+        protected void onCreate (Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_payments);
+            Intent intent = getIntent();
+            String amount = intent.getStringExtra("amount");
+            final Dialog dialog = new Dialog(context);
+            dialog.setContentView(R.layout.activity_user_information);
+            WindowManager.LayoutParams params = dialog.getWindow().getAttributes();
+            dialog.getWindow().setAttributes((WindowManager.LayoutParams) params);
+            final EditText namecard = (EditText) dialog.findViewById(R.id.Cardname);
+            final EditText creditcard = (EditText) dialog.findViewById(R.id.creditcard);
+            final EditText securitycode = (EditText) dialog.findViewById(R.id.SecurityCode);
+            final EditText month = (EditText) dialog.findViewById(R.id.month);
+            final EditText year = (EditText) dialog.findViewById(R.id.year);
+            Button order = (Button) dialog.findViewById(R.id.Submitorder);
+            order.setText("Complete Order ( Total " + amount + ")");
+            // if button is clicked, close the custom dialog
+            order.setOnClickListener(new View.OnClickListener() {
+                public void onClick(View v) {
+                    if ((namecard.getText().toString().isEmpty()) || (creditcard.getText().toString().isEmpty()) || (securitycode.getText().toString().isEmpty()) || (month.getText().toString().isEmpty())) {
+                        Toast.makeText(getApplicationContext(), "Please insert the data", Toast.LENGTH_SHORT).show();
+                    }
+                    else
+                    {
+                        Intent intent;
+                        intent = new Intent(com.arthi.moviebooking.PaymentsActivity.this, UserInformation.class);
+                        // once confirmation page is complete, switch MainActivity Class
+                        // Confirmation starts after PaymentInformation
+                        startActivity(intent);
+                        dialog.dismiss();
+                    }
+                }
+            });
+            dialog.show();
+
+        }
+
 
     }
-}
+
---
+++ app/src/main/java/com/arthi/moviebooking/UserInformation.java
@@ -0,0 +1,14 @@
+package com.arthi.moviebooking;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserInformation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_information);
+    }
+}
